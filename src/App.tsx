aa0925f77565055639fcbf477bbdd94d743483d2
Semih <semih.turkoglu@student.vinci.be>
2024-05-10 00:51:02 +0200

    feat: adding theme support

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,43 @@
 import "./App.css";
 
-import { FC, ReactElement } from "react";
+import { FC, ReactElement, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { authSelectors } from "./containers/auth/selectors";
 import Header from "./Components/Header";
 import Playlists from "./Components/Playlists";
 import SearchBar from "./Components/SearchBar";
+import { ThemeContext } from "./Components/ThemeContext";
 
 const App: FC = (): ReactElement => {
   const accessToken = useSelector(authSelectors.getAccessToken);
+  const [theme, setTheme] = useState(
+    window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"
+  )
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    root.style.setProperty(
+      "--color-background",
+      theme === "light" ? "var(--color-background-light)" : "var(--color-background-dark)"
+    );
+    root.style.setProperty(
+      "--color-text",
+      theme === "light" ? "var(--color-text-light)" : "var(--color-text-dark)"
+    );
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
 
   return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
     <div>
       <Header accessToken={accessToken}/>
       <SearchBar />
       <Playlists accessToken={accessToken}/>
     </div>
+      </ThemeContext.Provider>
   );
 };
 
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,27 +1,43 @@
-import React, { FC, ReactElement } from 'react';
-import { useGetUserQuery } from '../api/apiSlice';
+import React, { FC, ReactElement, useContext } from "react";
+import { useGetUserQuery } from "../api/apiSlice";
+import { ThemeContext } from "./ThemeContext";
 
 interface HeaderProps {
   accessToken: string | undefined;
 }
 
-const Header: FC<HeaderProps> = ({accessToken}): ReactElement => {
-
-  const { data: user, isLoading, isError } = useGetUserQuery(undefined,{
+const Header: FC<HeaderProps> = ({ accessToken }): ReactElement => {
+  const {
+    data: user,
+    isLoading,
+    isError
+  } = useGetUserQuery(undefined, {
     skip: !accessToken
-});
+  });
+
+  const {theme, toggleTheme} = useContext(ThemeContext);
 
   if (isLoading) {
     return <div>Loading...</div>;
   }
-  if(isError) {
-    return <h1>You are currently not logged in</h1>
+  if (isError) {
+    return <h1>You are currently not logged in</h1>;
   }
 
   return (
-    <header>
-      <h1>Welcome, {user?.display_name}</h1>
-    </header>
+    <div className="row header-container">
+      <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
+        <header>
+          <h1>Welcome to Listify, {user?.display_name} !</h1>
+        </header>
+        <div className="custom-control custom-switch">
+          <input type="checkbox" className="custom-control-input" id="darkSwitch" checked={theme === "dark"} onChange={toggleTheme}/>
+          <label className="custom-control-label" htmlFor="darkSwitch">
+            Dark Mode
+          </label>
+        </div>
+      </div>
+    </div>
   );
 };
 
